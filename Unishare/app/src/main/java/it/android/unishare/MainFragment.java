91b3974f194a139a260708ca1972a00291a31269
Mastercava <mastercava@hotmail.it>
2015-07-10 16:42:16 +0200

    ViewFlipper

--- Unishare/app/src/main/java/it/android/unishare/MainFragment.java
+++ Unishare/app/src/main/java/it/android/unishare/MainFragment.java
@@ -4,13 +4,14 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainFragment extends Fragment implements ViewInitiator {
 	
@@ -19,6 +20,15 @@ public class MainFragment extends Fragment implements ViewInitiator {
 	private MainActivity activity;
 	private View view;
 
+	private Integer [] viewFlipperArray = {
+			R.layout.viewflipper_opinions,
+			R.layout.viewflipper_books
+	};
+	private ArrayList<Integer> viewFlipperElements;
+
+	public MainFragment() {
+		viewFlipperElements = new ArrayList<Integer>(Arrays.asList(viewFlipperArray));
+	}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -41,7 +51,20 @@ public class MainFragment extends Fragment implements ViewInitiator {
 		Utilities.loadImage(campusImageView, "campus.jpg", activity.getApplicationContext());
 
 		TextView dashNews = (TextView) view.findViewById(R.id.dashNews);
-		if(activity.getNews()!=null )dashNews.setText(activity.getNews());
+		//if(activity.getNews()!=null )dashNews.setText(activity.getNews());
+
+		LayoutInflater factory = LayoutInflater.from(activity);
+
+		ViewFlipper viewFlipper = (ViewFlipper) view.findViewById(R.id.viewFlipper);
+		Collections.shuffle(viewFlipperElements);
+		for(Integer index : viewFlipperElements) {
+			viewFlipper.addView(factory.inflate(index, null));
+		}
+
+		viewFlipper.setInAnimation(activity, R.anim.slide_in_from_left);
+		viewFlipper.setOutAnimation(activity, R.anim.slide_out_to_right);
+		viewFlipper.setFlipInterval(6000);
+		viewFlipper.startFlipping();
 
 	}
     
